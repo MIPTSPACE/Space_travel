e9e83a2395003043a864d08dcbdcddff02bc904c
Serhiy <Onspeeds@bifmir.net>
2015-04-19 20:35:31 +0300

    data some changes

--- Serg /planetsdata/main.cpp
+++ Serg /planetsdata/main.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include<stdio.h>
 #include <stdlib.h>
+#include <sstream>
 
 using namespace std;
 
@@ -12,17 +13,39 @@ private:
         void getdata(string h,int i){
             data[i]=h;
         }
-
+        void datanull(){
+        for (int i=0;i<200;i++)
+            data[i]="$%^";
+        }
+        string convertInt(int number)
+        {
+           stringstream ss;
+           ss << number;
+           return ss.str();
+        }
+        bool planetdataexist(char* q){
+            ifstream f;
+             f.open(q,ios_base::in);
+             if (f.is_open()==0 ) {
+                 f.close();
+                 return false;
+             }
+             else{
+                 f.close();
+                 return true;
+             }
+        }
 
 public:
         string data[200];
 
-        void downloaddata(char *q){
+        void downloaddataplanet(char *q){
            string s="";
            ifstream f;
 
            s+=q;
            f.open(s.c_str(),ios_base::in);
+           datanull();
            if (f.is_open()==0 ) cout<<"there is not a file with such planet"<<endl;
            else{
                 s="";
@@ -46,21 +69,91 @@ public:
 
                 f.close();
               }
+        void changedataplanet(string h,int what,char* planet){
+            ofstream f1;
+            downloaddataplanet(planet);
+            data[what].clear();
+            std::string mystr;
+            mystr=convertInt(what);
+            mystr+="***";
+            data[what]=mystr+h+"\n";
+            string s;
+            s=planet;
+            f1.open(s.c_str(),ios_base::out);
+            for(int i=1;i<200;i++){
+                if(data[i]!="$%^"){
+                        if(i==what) f1.write(data[i].c_str(),data[i].length());
+                        else{
+                        mystr.clear();
+                        mystr=convertInt(i);
+                        data[i]=mystr+"***"+data[i]+"\n";
+                        f1.write(data[i].c_str(),data[i].length());
+                    }
 
-
-
-
-        void changedata(string h,int i){
+                }
+            }
+            f1.close();
 
         }
+        void deletesomedata(int what,char*planet){
+            if (!planetdataexist(planet)) cout<<"there is not such file"<<endl;
+            else{
+
+                    string s;
+                    s=planet;
+
+
+                    downloaddataplanet(planet);
+                    ofstream f;
+                     f.open(s.c_str(),ios_base::out);
+                    data[what]="$%^";
+                    for(int i=1;i<200;i++){
+                        if(data[i]!="$%^"){
+                            s.clear();
+                            s=convertInt(i);
+                            data[i]=s+"***"+data[i]+"\n";
+                           f.write(data[i].c_str(),data[i].length());
+                            }
+
+                    }
+
+                 f.close();
+             }
+
+        }
+        void deletedataplanet(char*planet){
+            cout<<remove(planet);
+        }
+   /*     void creatdataplanet(char *planet,int argc,char *arg){
+            if (planetdataexist(planet)) cout <<"error"<<endl;
+            else{
+                datanull();
+                string s=planet;
+                ofstream f;
+                 f.open(s.c_str(),ios_base::out);
+
+               for(int i=1;i<argc;i++){
+                   s.clear();
+                   s=convertInt(i);
+                   data[i]=s+"***"+argv[i]+"\n";
+                   f.write(data[i].c_str(),data[i].length());
+                   data[i].clear();
+                   data[i]=argv[i];
+               }
+
+            }
+        }*/
 };
+
 int main(int argc, char *argv[])
 {   Planets_data A;
-
-   A.downloaddata(argv[1]);
-    int i;
-    for (i=0;i<23;i++)
-          cout<<A.data[i]<<endl;
+   // A.downloaddata(argv[1]);
+    //A.deletedata(13,argv[1]);
+  //   A.creatdataplanet(3,1,"hello");
+  // A.changedata("jjjj",2,argv[1]);
+   // int i;
+   // for (i=0;i<23;i++)
+     //     cout<<A.data[i]<<endl;
 
     return 0;
 }
