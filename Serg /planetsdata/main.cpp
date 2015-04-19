79b1137383fdf053f3fcd1092c62b12b758b569a
Serhiy <Onspeeds@bifmir.net>
2015-04-19 14:53:22 +0300

    working with data

--- Serg /planetsdata/main.cpp
+++ Serg /planetsdata/main.cpp
@@ -1,40 +1,69 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <fstream>
 #include<stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
 class Planets_data{
+private:
+        void getdata(string h,int i){
+            data[i]=h;
+        }
+
 
 public:
-        string data[23];
+        string data[200];
 
-        void getdata(string h,int i){
-             data[i]=h;
-        }
+        void downloaddata(char *q){
+           string s="";
+           ifstream f;
+
+           s+=q;
+           f.open(s.c_str(),ios_base::in);
+           if (f.is_open()==0 ) cout<<"there is not a file with such planet"<<endl;
+           else{
+                s="";
+                int i;
+
+                string::size_type cheker;
+                while (!f.eof()){
+                      getline(f,s);
+                      cheker=s.find("***");
+                      if (cheker != string::npos){
+                          i=atoi((s.substr(0,cheker)).c_str());
+                          s=s.substr(i+3,s.length());
+                          s=s.substr(s.find_first_not_of(" "),s.length());
+                          cout<<"i'm here"<<endl;
+                          getdata(s,i);
+                      }
+                      s.clear();
+
+                      }
+
+                }
+
+                f.close();
+              }
+
+
+
+
         void changedata(string h,int i){
 
         }
 };
 int main(int argc, char *argv[])
 {   Planets_data A;
-    //read data
-    ifstream f;
-   // s+=argv[1];
-    f.open(s.c_str(),ios_base::in);
-    if (f.is_open()==0 ) cout<<"there is not a file with such planet"<<endl;
-    else{
-        s="";
-        for (int i=0;i<23;i++){
-            getline(f,s);
-            A.getdata(s,i);
-            }
 
-            f.close();
-            cout<<"I'm here"<<endl;
+    A.downloaddata(argv[1]);
 
-}
+    int i;
+    for (i=0;i<23;i++)
+          cout<<A.data[i]<<endl;
+
     return 0;
 }
 
