6392ab15f96e9a06dfd574a03d8f1cd9e934bd9b
Serhiy <Onspeeds@bifmir.net>
2015-04-20 20:47:44 +0300

    Some changes

--- Serg /Sun_system/Sunsys/main.cpp
+++ Serg /Sun_system/Sunsys/main.cpp
@@ -2,168 +2,131 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
 class Sunsystem_body{
 private:
-double stod(const string &strInput){
-    double dbl_one = 0;
-   double dbl_two = 0;
-   double dbl_final = 0;
-   int strlen;
-   bool dec_pt = false;
-   int nums_before_dec = 0;
-   int nums_after_dec = 0;
-   strlen = strInput.length();
-  /* Check whether the string can be transformed into a number */
-  if(strInput[0] == '0' && strInput[1] == '0')
-  {
-  // invalid number !
-  return 0;
-  }
-  for(int i = 0; i < strlen; i++)
-  { if(strInput[i] == '0' || strInput[i] == '1' || strInput[i] == '2' || strInput[i] == '3' || strInput[i] == '4' || strInput[i] == '5' || strInput[i] == '6' || strInput[i] == '7' || strInput[i] == '8' || strInput[i] == '9')
-      {
-     // valid number
-     } else if(strInput[i] == '.'){
-     if(dec_pt)
-     {
-     // there was already a decimal point counted
-     // invalid number !
-     return 0;
-     } else {
-     dec_pt = true; // increment by one
-     }
-     } else {
-     // invalid number !
-     return 0;
-     }
-     }
-  /* Convert the number */
- // STEP 1: Calculate the amount of numbers before/after the decimal point (if there's one)
-  if(dec_pt) // if there's a decimal point in the number
- {
- for(int i = 0; i < strlen; i++)
- {
- if(strInput[i+1] != '.')
- {
- nums_before_dec++;
- } else {
- nums_before_dec++;
- break;
- }
- }
- nums_after_dec = strlen-nums_before_dec;
- nums_after_dec -= 1;
- } else {
- // This piece of code was added later as a BUGFIX !
- // Now the STOD-function is working 100% perfect !!!
- nums_after_dec = 0;
- nums_before_dec = strlen;
- }
- // STEP 2: Convert the string to a real number
- for(int i = 0; i < nums_before_dec; i++)
- {
- switch(strInput[i])
- {
- case '0':
- dbl_one += 0 * apow(10, (nums_before_dec - i));
- break;
- case '1':
- dbl_one += 1 * apow(10, (nums_before_dec - i));
- break;
- case '2':
- dbl_one += 2 * apow(10, (nums_before_dec - i));
- break;
- case '3':
- dbl_one += 3 * apow(10, (nums_before_dec - i));
- break;
- case '4':
- dbl_one += 4 * apow(10, (nums_before_dec - i));
- break;
- case '5':
- dbl_one += 5 * apow(10, (nums_before_dec - i));
- break;
- case '6':
- dbl_one += 6 * apow(10, (nums_before_dec - i));
- break;
- case '7':
- dbl_one += 7 * apow(10, (nums_before_dec - i));
- break;
- case '8':
- dbl_one += 8 * apow(10, (nums_before_dec - i));
- break;
- case '9':
- dbl_one += 9 * apow(10, (nums_before_dec - i));
- break;
- default:
- // invalid number !
- return 0;
- }
- }
- dbl_one = dbl_one / 10; // little fix
-for(int i = 0; i < nums_after_dec; i++)
-{
-switch(strInput[i + nums_before_dec + 1])
-{
-case '0':
-dbl_two += (0 / apow(10, i+1));
-break;
-case '1':
-dbl_two += (1 / apow(10, i+1));
-break;
-case '2':
-dbl_two += (2 / apow(10, i+1));
-break;
-case '3':
-dbl_two += (3 / apow(10, i+1));
-break;
-case '4':
-dbl_two += (4 / apow(10, i+1));
-break;
-case '5':
-dbl_two += (5 / apow(10, i+1));
-break;
-case '6':
-dbl_two += (6 / apow(10, i+1));
-break;
-case '7':
-dbl_two += (7 / apow(10, i+1));
-break;
-case '8':
-dbl_two += (8 / apow(10, i+1));
-break;
-case '9':
-dbl_two += (9 / apow(10, i+1));
-break;
-default:
-// invalid number !
-return 0;
-}
-}
-// STEP 3: Return the converted string as a double:
-dbl_final = dbl_one + dbl_two;
-return dbl_final;
-}
-/* This function 'apow' raises x to the power of y, it's a dependency of 'stod' */
-double apow(const float &x, int y)
-{
-double result = 1;
-if(y == 0)
-return result;
-if(y < 0)
-{
-y = -y;
-for(int i = 0; i < y; i++)
-result = result * x;
-return 1/result;
-}
-for(int i = 0; i < y; i++)
-result = result * x;
-return result;
-}
-    void changenumberofplanet(){
+    int stod(const char *s, double *v)
+    {
+        enum signs {NEG = -1, NONE = 0, POS = 1};
+
+        int i, dot, x, bError, bExp;
+        enum signs sign, esign;
+        double d, f;
+
+        sign = esign = NONE;
+        i = x = bError = bExp = 0;
+        dot = -1;
+        d = f = 0.0;
+        while (!bError)
+        {
+            switch (*s)
+            {
+            case '+':
+                if (!bExp)
+                {
+
+
+                    if (sign)
+                        bError = 1;
+                    else sign = POS;
+                }
+                else
+                {
+
+
+                    if (esign)
+                        bError = 1;
+                    else esign = POS;
+                }
+                   if (i && isdigit(*(s - 1))) bError = 1;
+                break;
+            case '-':
+
+                if (!bExp)
+                {
+
+
+                    if (sign)
+                        bError = 1;
+                    else sign = NEG;
+                }
+                else
+                {
+
+
+                    if (esign)
+                        bError = 1;
+                    else esign = NEG;
+                }
+
+
+
+                if (i && isdigit(*(s - 1))) bError = 1;
+                break;
+            case '.':
+
+
+
+
+                if (!bExp)
+                {
+
+                    if (dot > -1) bError = 1;
+                    else dot++;
+                }
+                else bError = 1;
+                break;
+            case 'e':
+            case 'E':
+
+
+
+                if (bExp) bError = 1;
+                else bExp = 1;
+                break;
+            default:
+
+
+
+                if (isdigit(*s))
+                {
+                    if (bExp)
+                        x = x*10 + (*s - '0');
+                    else
+                    {
+                        if (dot > -1)
+                        {
+                            f = f*10 + (*s - '0');
+                            dot++;
+                        }
+                        else d = d*10 + (*s - '0');
+                    }
+                }
+                else
+                {
+
+                    if (*s == '\0')
+                    {
+
+                        *v = d + f*pow(10, -dot);
+                        if (x != 0.0) *v *= pow(10, x*(sign == NEG ? -1:1));
+                        if (sign == NEG) *v *= -1;
+                        return 0;
+                    }
+                    else bError = 1;
+                }
+            }
+            s++;
+            i++;
+        }
+
+
+        return i;
+    }
+void changenumberofplanet(){
         for(int i=0;i<plmas.size();i++)
             plmas[i].number=i;
     }
@@ -319,19 +282,19 @@ short planet_from_file(string path){//-1 файл не найден 0--ok
             myplanet.name=s;
             if (f.eof()) break;
             getline(f,s);
-            myplanet.posx=stod(s);
+            stod(s.c_str(),&myplanet.posx);
             if (f.eof()) break;
             getline(f,s);
-            myplanet.posy=stod(s);
+            stod(s.c_str(),&myplanet.posy);
             if (f.eof()) break;
             getline(f,s);
-            myplanet.mass=stod(s);
+            stod(s.c_str(),&myplanet.mass);
             if (f.eof()) break;
             getline(f,s);
-            myplanet.speedx=stod(s);
+            stod(s.c_str(),&myplanet.speedx);
             if (f.eof()) break;
             getline(f,s);
-            myplanet.speedy=stod(s);
+            stod(s.c_str(),&myplanet.speedy);
             addplanet(myplanet.name,myplanet.posx,myplanet.posy,myplanet.mass,myplanet.speedx,myplanet.speedy);
 
         }
@@ -346,12 +309,7 @@ short planet_from_file(string path){//-1 файл не найден 0--ok
 int main()
 {
     Sunsystem_body A;
-    A.addplanet("Merqury",123,123,2,3,4);
-    A.addplanet("Venues",123,123,2,3,4);
-    A.addplanet("Earth",123,123,2,3,4);
-    A.addplanet("Mars",123,123,2,3,4);
-    A.addplanet("Mars",123,123,2,3,4);
-    A.changeplanetdata("Mars",1,1.255);
+    A.planet_from_file("data.txt");
     cout<<"!!!!"<<A.plmas.size()<<"!!!!!!!!!!!!!!";
 
     return 0;
