104c3ae130d90dffb1b6a698c4b568cb2f819e07
liviniuk <viktor.liviniuk@gmail.com>
2015-05-16 00:21:12 +0300

    Kepler determination are debugged, other on debug stage

--- Serg /pars/myparser.h
+++
@@ -1,4 +0,0 @@
-QStringList splitintowords(QString s,QString s1);
-struct mydate dateformat(QString s);
-QString myanswer(QString s);
-QString Parsecommand(QString str);
--- Serg /pars/myqmap.h
+++
@@ -1,5 +0,0 @@
-#ifndef MYQMAP_H
-#define MYQMAP_H
-
-
-#endif // MYQMAP_H
